2999839021ae9f85c36c5a457ae46bf29b9ef300
Gust4v0M <gustavomorgado6@gmail.com>
2024-08-16 17:56:26 -0300

    Home componet done

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -13,16 +13,31 @@ import { subscribe } from 'diagnostics_channel';
 })
 export class HomeComponentComponent implements OnInit {
 
-  food: string = 'Spaghetti'
+  food: string = 'Brownies'
   img!: any;
+  nome!: string;
+  recipiesInfo: {[key: string]: any} = {}
   constructor(private service: InfoRecipiesService) {}
 
   ngOnInit(){
     this.service.getInfoRecipies(this.food)
     .subscribe((res: any) =>{
-      console.log(res)
-      this.img = res.meals[1].strMealThumb
-     
+
+      this.img = res.meals[0].strMealThumb
+      this.nome = res.meals[0].strMeal
     })
+
+    const recipies = ['Lancashire','Dumpling', 'Pancakes', 'Brownies']
+    recipies.forEach(res => this.getRecipiesInfo(res))
+  }
+
+
+  getRecipiesInfo(res: any){
+    this.service.getInfoRecipies(res).
+    subscribe((dados: any)=>{
+      this.recipiesInfo[res] = dados.meals[0]
+      console.log(this.recipiesInfo[res])
+    }
+  )
   }
 }
