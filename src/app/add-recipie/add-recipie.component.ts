d5b27930dca5b31f2dd752c69f1de1afeb9ce4f5
Gust4v0M <gustavomorgado6@gmail.com>
2024-08-29 23:55:28 -0300

    Test faleid

--- src/app/add-recipie/add-recipie.component.ts
+++ src/app/add-recipie/add-recipie.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipie',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-recipie.component.html',
-  styleUrl: './add-recipie.component.css'
+  styleUrl: './add-recipie.component.css',
 })
-export class AddRecipieComponent {
+export class AddRecipieComponent implements OnInit {
+  formulario!: FormGroup;
+  receitas: any;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      name: ['vodin'],
+      categories: ['cdk'],
+      ingredients: ['null'],
+      description: ['sdsdsd'],
+    });
+
+    this.receitas = [
+      {
+        id: 1,
+        name: '',
+        category: '',
+        ingredients: '',
+        description: '',
+      },
+    ];
+    this.pushRecipies();
+  }
+
+  pushRecipies() {
+   
+    this.receitas[0].name = this.formulario.value.name;
+    this.receitas[0].category = this.formulario.value.categories;
+    this.receitas[0].ingredients = this.formulario.value.ingredients;
+    this.receitas[0].description = this.formulario.value.description;
+  
+    console.log(this.receitas);
+  }
 }
